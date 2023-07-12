375d91709bdc90f1e7b2c6ec392f054e7f9f27a0
Gabriel Garcia <Gabriel.garciag496@gmail.com>
2023-07-12 15:10:10 -0700

    More info about game

--- Gadron.py
+++ Gadron.py
@@ -1,1 +1,17 @@
-b
+import random
+import time
+
+room = 1
+#Text Messanger base
+
+#Building Enviorment
+def build_gen():
+TR_room = room
+TL_room = room
+BR_room = room
+BL_room = room
+
+    
+    
+build_gen()
+    
